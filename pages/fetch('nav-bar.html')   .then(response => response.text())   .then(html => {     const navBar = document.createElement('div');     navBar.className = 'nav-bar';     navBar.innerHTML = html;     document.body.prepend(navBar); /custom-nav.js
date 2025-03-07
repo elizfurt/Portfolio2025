f3c5855397b93621cf3cb79e25ad2e732b38d556
Liz Furtado <81473881+elizfurt@users.noreply.github.com>
2025-03-07 16:10:30 -0500

    Delete pages/fetch('nav-bar.html')   .then(response => response.text())   .then(html => {     const navBar = document.createElement('div');     navBar.className = 'nav-bar';     navBar.innerHTML = html;     document.body.prepend(navBar);  directory

--- pages/fetch('nav-bar.html')   .then(response => response.text())   .then(html => {     const navBar = document.createElement('div');     navBar.className = 'nav-bar';     navBar.innerHTML = html;     document.body.prepend(navBar); /custom-nav.js
+++
@@ -1,9 +0,0 @@
-fetch('nav-bar.html')
-  .then(response => response.text())
-  .then(html => {
-    const navBar = document.createElement('div');
-    navBar.className = 'nav-bar';
-    navBar.innerHTML = html;
-    document.body.prepend(navBar); // Adds it to the top of the body
-  })
-  .catch(error => console.error('Error loading nav-bar:', error));
