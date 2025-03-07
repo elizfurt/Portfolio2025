fb84c935c8d5c0fe6a69c859e3947fd6aa8f42b4
Liz Furtado <81473881+elizfurt@users.noreply.github.com>
2025-03-07 16:10:08 -0500

    Create custom-nav.js

---
+++ pages/fetch('nav-bar.html')   .then(response => response.text())   .then(html => {     const navBar = document.createElement('div');     navBar.className = 'nav-bar';     navBar.innerHTML = html;     document.body.prepend(navBar); /custom-nav.js
@@ -0,0 +1,9 @@
+fetch('nav-bar.html')
+  .then(response => response.text())
+  .then(html => {
+    const navBar = document.createElement('div');
+    navBar.className = 'nav-bar';
+    navBar.innerHTML = html;
+    document.body.prepend(navBar); // Adds it to the top of the body
+  })
+  .catch(error => console.error('Error loading nav-bar:', error));
